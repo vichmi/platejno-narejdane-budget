acf99e4715cf0dbb9f59a323f59d79bcbdc266cc
Victor Tanov <vichmibg@mail.bg>
2023-10-09 23:36:17 +0300

    Add templates

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import os
 import sys
 import tkinter as tk
 from win32com import client
+import json
 
-templates = [
-    {
-        'name': 'test'
-    }
-]
+templates = []
+
+temapltes_file = open('./bin/templates.json', 'r')
+templates = json.loads(temapltes_file.read())
 
 ADDRESSEE = [
     {
@@ -46,8 +46,11 @@ window = tk.Tk(className='Accountant')
 window.geometry('800x600')
 
 def fill_excel(*args):
-    if payment_type.get() == '':
-        error_label = tk.Label(window, text = 'Избери метод на плащане')
+    if payment_type.get() == '--Choose option--':
+        error_label = tk.Label(window, text = 'Избери метод на плащане', fg='#ff1100')
+        error_label.grid(row=0, column=10)
+        return
+    print(payment_type.get())
     addresse = [adr for adr in ADDRESSEE if adr['short_name'] == payment_type.get()][0]
     EXCEL_FILE = [f for f in os.listdir('.') if f == 'obrazec.xlsx'][0]
 
@@ -96,12 +99,6 @@ payment_type.set('--Choose option--')
 type_of_payment_drop = tk.OptionMenu(window, payment_type, *[adr['short_name'] for adr in ADDRESSEE])
 type_of_payment_drop.grid(row = 0, column=1)
 
-templates_options = tk.StringVar()
-templates_options.set('Шаблони')
-
-templates_options_menu = tk.OptionMenu(window, templates_options, *[t['name'] for t in templates])
-templates_options_menu.grid(column=9, row=0)
-
 
 class InputBox:
     def __init__(self, label_text, col, row):
@@ -111,6 +108,12 @@ class InputBox:
         self.text_label.grid(column = col, row = row)
         self.text_entry.grid(column = col+1, row = row)
 
+    def delete_value(self):
+        self.text_entry.delete(0, len(self.text_var.get()))
+
+    def set_value(self, value):
+        self.text_entry.insert(0, value)
+
 firm = InputBox('Задължено лице', 0, 1)
 firm_eik = InputBox('ЕИК/код по БУЛСТАТ', 0, 2)
 nareditel = InputBox('Наредител', 0, 3)
@@ -119,6 +122,49 @@ payment_sum = InputBox('Сума', 0, 5)
 
 download_button = tk.Button(window, text = 'Свали', command=fill_excel)
 download_button.grid(column=1, row=6)
+
+def add_template():
+    options = {
+        'name': save_template.text_var.get(),
+        'firm_name': firm.text_var.get(),
+        'firm_eik': firm_eik.text_var.get(),
+        'nareditel': nareditel.text_var.get(),
+        'nareditel_iban': nareditel_iban.text_var.get(),
+        'payment_type': payment_type.get()
+    }
+    templates.append(options)
+
+    with open('./bin/templates.json', 'w') as f:
+        json.dump(templates, f, indent = 4)
+    templates_options_menu = tk.OptionMenu(window, templates_options, *[t['name'] for t in templates])
+        
+
+save_template = InputBox('Запази шаблон:', 0, 8)
+save_template_btn = tk.Button(window, text = 'Запази', command=add_template)
+save_template_btn.grid(column=2, row=8)
+
+templates_options = tk.StringVar()
+templates_options.set('Шаблони')
+
+def set_template(*args):
+
+    template = [t for t in templates if t['name'] == templates_options.get()][0]
+
+    firm.delete_value()
+    firm_eik.delete_value()
+    nareditel.delete_value()
+    nareditel_iban.delete_value()
+
+    firm.set_value(template['firm_name'])
+    firm_eik.set_value(template['firm_eik'])
+    nareditel.set_value(template['nareditel'])
+    nareditel_iban.set_value(template['nareditel_iban'])
+    payment_type.set(template['payment_type'])
+    print(template)
+
+templates_options_menu = tk.OptionMenu(window, templates_options, *[t['name'] for t in templates])
+templates_options.trace('w', set_template)
+templates_options_menu.grid(column=9, row=0)
 
 window.mainloop()
 
