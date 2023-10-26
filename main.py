35fb2981dabfb95fc6f010d35710e1750fd677eb
Victor Tanov <vichmibg@mail.bg>
2023-10-26 11:42:24 +0300

    Removed spaghetti code and made it oop

--- main.py
+++ main.py
@@ -4,32 +4,11 @@ import tkinter as tk
 import json
 import datetime
 
-templates = []
-
-temapltes_file = open('./templates.json', 'r')
-templates = json.loads(temapltes_file.read())
-templates = sorted(templates, key = lambda t: t['name'].lower())
-
-class ErrorMessage:
-
-    def __init__(self):
-        self.label = tk.Label(window, text='')
-
-    def set_msg(self, text, element):
-        self.label = tk.Label(window, text=text, fg='#ff1100')
-        row = element.grid_info()['row']
-        column = element.grid_info()['column'] + 1
-        self.label.grid(row=row, column=column)
-
-    def hide(self):
-        self.label.grid_remove()
-
-def is_float(string):
-    try:
-        float(string)
-        return True
-    except ValueError:
-        return False
+# Define constants
+EXCEL_FILE = 'sample.xlsx'
+TEMPLATE_FILE = './templates.json'
+COLUMNS = 'a b c d e f g h i j k l m n o p q r s t u v w x y z aa ab ac ad ae af ag ah ai aj ak'.split(' ')
+FILE_PATH = './templates.json'
 
 ADDRESSEE = [
     {
@@ -61,153 +40,166 @@ ADDRESSEE = [
         'osnovanie_row2': ''
     }
 ]
-COLUMNS = 'a b c d e f g h i j k l m n o p q r s t u v w x y z aa ab ac ad ae af ag ah ai aj ak'.split(' ')
 
-window = tk.Tk(className='Платежно Нареждане Бюджет')
-window.geometry('800x600')
+# Load templates from the JSON file
+def load_templates():
+    try:
+        with open(FILE_PATH, 'r') as TEMPLATE_FILE:
+            return json.load(TEMPLATE_FILE)
+    except:
+        return [{'name': '--Няма създадени шаблони--'}]
 
-EXCEL_FILE = [f for f in os.listdir('.') if f == 'obrazec.xlsx'][0]
+# Save templates to the JSON file
+def save_templates(templates):
+    with open(FILE_PATH, 'w') as template_file:
+        json.dump(templates, template_file, indent=4)
 
+class PaymentBudgetApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title('Платежно Нареждане Бюджет')
+        self.templates = load_templates()
+        self.create_gui()
 
-error_message = ErrorMessage()
-
-def fill_cells(text, row_num, sheet):
-    for i in range(len(text)):
-        sheet[f'{COLUMNS[i+1]}{row_num}'] = text[i]
-
-def fill_excel(*args):
-    error_message.hide()
-    if payment_type.get() == '--Choose option--':
-        error_message.set_msg('Избери метод на плащане', type_of_payment_drop)
-        return
-    if payment_sum.check_number() is False:
-        return
-    addresse = [adr for adr in ADDRESSEE if adr['short_name'] == payment_type.get()][0]
-
-    wb = openpyxl.load_workbook(EXCEL_FILE)
-    sheet = wb.active
-    # IBAN
-    fill_cells(addresse['iban'], 21, sheet)
+    def create_gui(self):
         
-    # OSNOVANIE 1
-    fill_cells(addresse['osnovanie_row1'], 27, sheet)
+        self.payment_type_label = tk.Label(self.root, text = 'Метод на плащане')
+        self.payment_type_label.grid(row = 0, column = 0)
+        self.payment_type_var = tk.StringVar()
+        self.payment_type_var.set('--Изберете метод--')
+        self.payment_type_option = tk.OptionMenu(self.root, self.payment_type_var, *[adr['short_name'] for adr in ADDRESSEE])
+        self.payment_type_option.grid(row = 0, column = 1)
 
-    # Firm
-    fill_cells(firm.var.get(), 37, sheet)
+        self.templates_var = tk.StringVar()
+        self.templates_var.set('--Няма създадени шаблони--')
+        self.templates_options = tk.OptionMenu(self.root, self.templates_var, *[t['name'] for t in self.templates], command = self.set_template)
+        self.templates_options.grid(row = 0, column = 2)
 
-    # Firm EIK
-    fill_cells(firm_eik.var.get(), 43, sheet)
+        def create_label_entry(title, row):
+            label = tk.Label(self.root, text = title)
+            label.grid(row = row, column = 0)
+            var = tk.StringVar()
+            entry = tk.Entry(self.root, textvariable = var)
+            entry.grid(row = row, column = 1)
+            return var
+        self.company = create_label_entry('Задължено лице',1)
+        self.comapy_eik = create_label_entry('ЕИК/код по БУЛСТАТ', 2)
+        self.nareditel = create_label_entry('наредител', 3)
+        self.nareditel_iban = create_label_entry('IBAN на наредителя', 4)
+        self.payment_sum = create_label_entry('Сума', 5)
+        
+        self.download_button = tk.Button(self.root, text = 'Свали', command=self.fill_excel)
+        self.download_button.grid(row = 6, column = 1)
 
-    # Nareditel
-    fill_cells(nareditel.var.get(), 46, sheet)
+        self.save_template = create_label_entry('Запази шаблон', 7)
+        self.save_temaplte_btn = tk.Button(self.root, text = 'Запази', command=self.add_template)
+        self.save_temaplte_btn.grid(row = 7, column = 2, sticky='W')
 
-    # Nareditel IBAN
-    fill_cells(nareditel_iban.var.get(), 49, sheet)
+        self.delete_template = tk.Button(self.root, text = 'Изтрий шаблон', command=self.delete_template, fg='red')
+        self.delete_template.grid(row = 8, column = 1)
+        self.error_var = tk.StringVar()
+        self.error_label = tk.Label(self.root, text = self.error_var.get(), fg='red')
 
-    # SUM
-    index = 0
-    coins = 0
-    sum_string = ''
-    print(sum_string)
-    if '.' not in payment_sum.var.get():
-        payment_sum.var.set(f'{payment_sum.var.get()}.00')
-    
-    sum_string = payment_sum.var.get().replace('.', '')
+    def update_optionmenu(self):
+        self.templates_options['menu'].delete(0, 'end')
+        for opt in self.templates:
+            self.templates_options['menu'].add_command(label=opt['name'], command=tk._setit(self.templates_var, opt['name']))
 
-    for i in range(len(sum_string) - 1, -1, -1):
-        find_index = COLUMNS.index('aj') - index - coins
-        sheet[f'{COLUMNS[find_index]}24'] = sum_string[i]
-        index += 1
+    def set_error(self, text, row, column):
+        self.error_var.set(text)
+        self.error_label.grid(row = row, column = column)
 
-    d = datetime.datetime.now()
-    file_name = f'{firm.var.get()} {payment_type.get()} {d.strftime("%d")}-{d.strftime("%m")}-{d.strftime("%Y")} {d.strftime("%H")}-{d.strftime("%M")}-{d.strftime("%S")}.xlsx'
-    wb.save(f'./FILES/{file_name}')
-    payment_sum.delete_value()
+    def remove_error(self):
+        self.error_var.set('')
+        self.error_label.grid_remove()
 
-payment_label = tk.Label(window, text = 'Метод на плащане ')
-payment_label.grid(row = 0, column=0)
+    def fill_cells(self, text, row_num, sheet):
+        for i, cell_value in enumerate(text):
+            sheet[f'{COLUMNS[i+1]}{row_num}'] = cell_value
 
-payment_type = tk.StringVar()
-payment_type.set('--Choose option--')
+    def fill_excel(self, *args):
+        wb = openpyxl.load_workbook(EXCEL_FILE) # Loading and set the sample.xlsx
+        sheet = wb.active
+        addresse = [adr for adr in ADDRESSEE if adr['short_name'] == self.payment_type_var.get()][0]
+        values_iterator = [
+            {
+                "text": addresse['iban'], 
+                "row_num": 21
+            },
+            {
+                "text": addresse['osnovanie_row1'], 
+                "row_num": 27
+            },
+            {
+                "text": self.company.get(), 
+                "row_num": 37
+            },
+            {
+                "text": self.comapy_eik.get(), 
+                "row_num": 43
+            },
+            {
+                "text": self.nareditel.get(), 
+                "row_num": 46
+            },
+            {
+                "text": self.nareditel_iban.get(), 
+                "row_num": 49
+            }
+        ]
 
-type_of_payment_drop = tk.OptionMenu(window, payment_type, *[adr['short_name'] for adr in ADDRESSEE])
-type_of_payment_drop.grid(row = 0, column=1)
+        for val in values_iterator:
+            self.fill_cells(val['text'], val['row_num'], sheet)
+
+        whole_sum = ''
+        if '.' not in self.payment_sum.get():
+            whole_sum = f'{self.payment_sum.get()}00'
+        else: whole_sum = self.payment_sum.get().replace('.', '')
+        index = 0
+        for i in range(len(whole_sum) - 1, -1, -1):
+            find_index = COLUMNS.index('aj') - index
+            sheet[f'{COLUMNS[find_index]}24'] = whole_sum[i]
+            index += 1
+
+        date = datetime.datetime.now()
+        wb.save(f'./files/{self.company.get()} {self.payment_type_var.get()} {date.strftime("%d")}-{date.strftime("%m")}-{date.strftime("%Y")}-{date.strftime("%H")}-{date.strftime("%M")}-{date.strftime("%S")}.xlsx')
+        # Setting IBAN
+
+    def add_template(self):
+        options = {
+            'name': self.save_template.get(),
+            'firm_name': self.company.get(),
+            'firm_eik': self.comapy_eik.get(),
+            'nareditel': self.nareditel.get(),
+            'nareditel_iban': self.nareditel_iban.get(),
+            'payment_type': self.payment_type_var.get()
+        }
+        self.templates.append(options)
+        self.templates = sorted(self.templates, key = lambda t: t['name'].lower())
+        save_templates(self.templates)
+        self.update_optionmenu()
+        self.templates_var.set(self.save_template.get())
+
+    def set_template(self, *args):
+        template = [t for t in self.templates if t['name'] == self.templates_var.get()][0]
+
+        self.company.set(template['firm_name'])
+        self.comapy_eik.set(template['firm_eik'])
+        self.nareditel.set(template['nareditel'])
+        self.nareditel_iban.set(template['nareditel_iban'])
+        self.payment_type_var.set(template['payment_type'])
+
+    def delete_template(self):
+        template_name = [t for t in self.templates if t['name'] == self.templates_var.get()][0]
+        if template_name['name'] == '--Няма създадени шаблони--': return
+        self.templates.remove(template_name)
+        save_templates(self.templates)
+        self.update_optionmenu()
+        self.templates_var.set('--Няма създадени шаблони--')
 
 
-class InputBox:
-    def __init__(self, label_text, col, row):
-        self.var = tk.StringVar()
-        self.label = tk.Label(window, text = label_text)
-        self.entry = tk.Entry(window, textvariable = self.var)
-        self.label.grid(column = col, row = row)
-        self.entry.grid(column = col+1, row = row)
-
-    def delete_value(self):
-        self.entry.delete(0, len(str(self.var.get())))
-
-    def set_value(self, value):
-        self.entry.insert(0, value)
-
-    def check_number(self):
-        if is_float(self.var.get()):
-            return True
-        else:
-            error_message.set_msg('Невалидна сума', self.entry)
-            return False
-
-firm = InputBox('Задължено лице', 0, 1)
-firm_eik = InputBox('ЕИК/код по БУЛСТАТ', 0, 2)
-nareditel = InputBox('Наредител', 0, 3)
-nareditel_iban = InputBox('IBAN на наредителя', 0, 4)
-payment_sum = InputBox('Сума', 0, 5)
-
-download_button = tk.Button(window, text = 'Свали', command=fill_excel)
-download_button.grid(column=1, row=6)
-
-def add_template():
-    options = {
-        'name': save_template.var.get(),
-        'firm_name': firm.var.get(),
-        'firm_eik': firm_eik.var.get(),
-        'nareditel': nareditel.var.get(),
-        'nareditel_iban': nareditel_iban.var.get(),
-        'payment_type': payment_type.get()
-    }
-    templates.append(options)
-    new_sorted_templates = sorted(templates, key = lambda t: t['name'].lower())
-
-    with open('./templates.json', 'w') as f:
-        json.dump(new_sorted_templates, f, indent = 4)
-    templates_options_menu = tk.OptionMenu(window, templates_options, *[t['name'] for t in new_sorted_templates])
-    templates_options_menu.grid(column=9, row=0)
-
-save_template = InputBox('Запази шаблон:', 0, 8)
-save_template_btn = tk.Button(window, text = 'Запази', command=add_template)
-save_template_btn.grid(column=2, row=8)
-
-templates_options = tk.StringVar()
-templates_options.set('Шаблони')
-
-def set_template(*args):
-
-    template = [t for t in templates if t['name'] == templates_options.get()][0]
-
-    firm.delete_value()
-    firm_eik.delete_value()
-    nareditel.delete_value()
-    nareditel_iban.delete_value()
-    payment_sum.delete_value()
-
-    firm.set_value(template['firm_name'])
-    firm_eik.set_value(template['firm_eik'])
-    nareditel.set_value(template['nareditel'])
-    nareditel_iban.set_value(template['nareditel_iban'])
-    payment_type.set(template['payment_type'])
-
-if len(templates) > 0: 
-    templates_options_menu = tk.OptionMenu(window, templates_options,  *[t['name'] for t in templates])
-    templates_options_menu.grid(column=9, row=0)
-
-templates_options.trace('w', set_template)
-
-window.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PaymentBudgetApp(root)
+    root.geometry('800x600')
+    root.mainloop()
